ff00ee0f71589a6fbc79ea71db03d895b28d3210
yannvr <yannvr@gmail.com>
2014-06-17 04:16:48 +0100

    cleanup

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,14 +6,7 @@ angular.module('fastTracks.controllers', [])
   .controller('SCCtrl', ['SoundCloud', '$scope', function(SoundCloud, $scope) {
         SoundCloud.startApp();
         $scope.addTrack = function(trackUrl) {
-            debugger;
             SoundCloud.embedTrack(trackUrl);
-//            debugger;
         };
         $scope.SCData = SoundCloud.SCData;
-//        $scope.apply();
-        $scope.test = {test: 43+'XXx', subtest: {val: 32}};
   }]);
-//  .controller('MyCtrl2', ['$scope', function($scope) {
-//
-//  }]);
